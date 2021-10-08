090cc993b84b5e098d28d154052880db92dfc3b9
d4v1d <63854819+rhee519@users.noreply.github.com>
2021-10-08 22:21:34 +0900

    Time: 84 ms (38.72%), Space: 43 MB (91.00%) - LeetHub

---
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,52 @@
+class Trie {
+    unordered_map<char, Trie*> children;
+    bool exist = false;
+public:
+    Trie() { }
+    
+    void insert(string word) {
+        int len = word.size();
+        Trie* curr = this;
+        for(int i = 0; i < len; i++) {
+            if(!(curr->children.count(word[i]))) 
+                curr->children[word[i]] = new Trie();
+            curr = curr->children[word[i]];
+        }
+        curr->exist = true;
+    }
+    
+    bool search(string word) {
+        Trie* curr = this;
+        int len = word.size();
+        for(int i = 0; i < len; i++) {
+            if(!(curr->children.count(word[i]))) {
+                // printf("word[%d] doesn't exist\n", i);
+                return false;
+            }
+            curr = curr->children[word[i]];
+        }
+        if(!(curr->exist)) return false;
+        return true;
+    }
+    
+    bool startsWith(string prefix) {
+        Trie* curr = this;
+        int len = prefix.size();
+        for(int i = 0; i < len; i++) {
+            if(!(curr->children.count(prefix[i]))) {
+                // printf("prefix[%d] doesn't exist\n", i);
+                return false;
+            }
+            curr = curr->children[prefix[i]];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
