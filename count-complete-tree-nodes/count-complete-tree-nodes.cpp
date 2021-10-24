c144a09bae1af202e3a742a34f43813006d0b9db
d4v1d <63854819+rhee519@users.noreply.github.com>
2021-10-24 20:05:38 +0900

    Time: 51 ms (26.36%), Space: 30.8 MB (64.22%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,23 +11,8 @@
  */
 class Solution {
 public:
-    int height = -1;
-    void getHeight(TreeNode* root) {
-        height++; 
-        if(root->left) getHeight(root->left);
-    }
-    
-    int findRightEnd(TreeNode* root, int node, int h) {
-        if(!root) return -1;
-        if(h == height) return node;
-        int right = findRightEnd(root->right, node*2+1, h+1);
-        if(right != -1) return right;
-        return findRightEnd(root->left, node*2, h+1);
-    }
-    
     int countNodes(TreeNode* root) {
         if(!root) return 0;
-        getHeight(root);
-        return findRightEnd(root, 1, 0);
+        return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
