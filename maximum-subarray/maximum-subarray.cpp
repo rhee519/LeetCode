7ed024e80ae20512a2f7efc0b08f3211193e5dfa
d4v1d <63854819+rhee519@users.noreply.github.com>
2021-11-25 11:07:42 +0900

    Time: 156 ms (17.79%), Space: 67.6 MB (91.35%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0, ans = INT_MIN;
-        for(int& num: nums) {
-            sum += num;
-            ans = max(ans, sum);
-            if(sum < 0) sum = 0;
+    int dnc(vector<int>& nums, int left, int right) {
+        if(left > right) return INT_MIN;
+        if(left == right) return nums[left];
+        int mid = (left+right)/2;
+        int max_left = dnc(nums, left, mid);
+        int max_right = dnc(nums, mid+1, right);
+        int part = 0, max_mid_left = 0, max_mid_right = 0;
+        for(int i = mid-1; i >= left; i--) {
+            part += nums[i]; max_mid_left = max(max_mid_left, part);
         }
-        return ans;
+        part = 0;
+        for(int i = mid+1; i <= right; i++) {
+            part += nums[i]; max_mid_right = max(max_mid_right, part);
+        }
+        int max_mid = nums[mid] + max_mid_left + max_mid_right;
+        
+        return max(max(max_left, max_right), max_mid);
+    }
+    int maxSubArray(vector<int>& nums) {
+        return dnc(nums, 0, nums.size()-1);
     }
 };
