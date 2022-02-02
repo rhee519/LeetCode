c844e779f285a6a6857b430a09df9b41a31260e4
d4v1d <63854819+rhee519@users.noreply.github.com>
2022-02-02 11:38:32 +0900

    Time: 46 ms (26.57%), Space: 8.7 MB (42.26%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        unordered_map<char, int> cnt;
+        int m = s.size(), n = p.size();
+        if(m < n) return ans;
+        
+        for(int i = 0; i < n; i++) {
+            cnt[s[i]]++;
+            cnt[p[i]]--;
+        }
+        
+        // sliding window
+        for(int i = 0; i+n <= m; i++) {
+            bool isAnagram = true;
+            for(char key = 'a'; key <= 'z'; key++) {
+                if(cnt[key]) {
+                    isAnagram = false;
+                    break;
+                }
+            }
+            if(isAnagram) ans.push_back(i);
+            if(i+n < m) {
+                cnt[s[i]]--;
+                cnt[s[i+n]]++;
+            }
+        }
+        return ans;
+    }
+};
