3abd83ba1a27ac8287c29a0cd16fc1e050ba95e0
d4v1d <63854819+rhee519@users.noreply.github.com>
2022-03-04 10:40:22 +0900

    Time: 198 ms (39.32%), Space: 13.9 MB (95.15%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -1,13 +1,10 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        glass = [[0 for _ in range(query_row+1)] for _ in range(query_row+1)]
-        glass[0][0] = poured
+        glass = [0] * (query_row+1)
+        glass[0] = poured
         
-        for i in range(query_row):
-            for j in range(i+1):
-                if glass[i][j] > 1:
-                    glass[i+1][j] += (glass[i][j]-1)/2.0
-                    glass[i+1][j+1] += (glass[i][j]-1)/2.0
-                    glass[i][j] = 1.0
-        # print(glass)
-        return min(glass[query_row][query_glass], 1)
+        for i in range(1, len(glass)):
+            for j in range(i, -1, -1): # from j=i to j=0
+                glass[j] = max(glass[j]-1, 0)/2 + max(glass[j-1]-1, 0)/2
+        
+        return min(glass[query_glass], 1)
