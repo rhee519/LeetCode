c02a3fc933e96ff463c38a109e54f235c67a1737
d4v1d <63854819+rhee519@users.noreply.github.com>
2022-07-01 21:19:43 +0900

    Time: 553 ms (18.67%), Space: 14.6 MB (32.84%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        ans = [None for _ in range(len(people))]
+        people.sort(key=lambda x: (-x[0], -x[1]))
+        # print(people)
+        while people:
+            h, k = people.pop()
+            # print(f'h: {h}, k: {k}')
+            pos = -1
+            for i in range(len(ans)):
+                if ans[i] == None or ans[i][0] >= h:
+                    pos += 1
+                if pos == k:
+                    ans[i] = [h, k]
+                    break
+            # print(f'ans: {ans}')
+        
+        return ans
